2c56b91a9fa7b3107498d2b194fca5df4ed3168c
Nazar Vynnytskyi <nvynnytskyi@jappware.com>
2023-03-01 15:33:19 +0200

    Update cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:4200',
+    specPattern: 'cypress/e2e/tests/**/*.cy.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
